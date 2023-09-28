c61bc597c27e107628a41ca33139fae5c071bd92
Tigabu Abriham <81282391+Tigabu-eng@users.noreply.github.com>
2023-09-28 14:50:27 +0300

    Update FIFO Page Replacement Algorithm.py

--- FIFO Page Replacement Algorithm.py
+++ FIFO Page Replacement Algorithm.py
@@ -1,5 +1,5 @@
-#FIFO page replacement algorithm implementation in python
-#Created By: Suman Adhikari
+#FIFO page replacement algorithm implementation in Python
+#Created By: Tigabu Abriham
 
 print("Enter the number of frames: ",end="")
 capacity = int(input())
